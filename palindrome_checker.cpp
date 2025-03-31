eebeaea0642405940d254e715d8e2dc010341f9b
Sanju <sanjana.b0831@gmail.com>
2025-03-31 17:25:01 +0530

    palindrome_checker

---
+++ palindrome_checker.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(string str) {
+    int left = 0, right = str.length() - 1;
+    while (left < right) {
+        if (str[left] != str[right])
+            return false;
+        left++;
+        right--;
+    }
+    return true;
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    if (isPalindrome(str))
+        cout << "It is a palindrome." << endl;
+    else
+        cout << "Not a palindrome." << endl;
+
+    return 0;
+}
